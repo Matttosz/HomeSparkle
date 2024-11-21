d808ef8d8c350005c5b07218787964a3f21a04dd
JVS453 <149633554+JVS453@users.noreply.github.com>
2024-11-21 16:58:47 -0300

    Inseri os pop-ups, atrelando eles as imagens. Depois vou revisar os textos, coloquei a coisa mais genérica possível. Ainda vou tentar encontrar uma forma de fechar quando alguém for clicar em outra imagem, pois quando você clica em outra imagem um pop-up fica sobre o outro. Ainda não troquei a parte de Meus Levatamentos pois queria focar nos pop-ups e estava demorando mais do que deveria. Mas vou fazer dar certo :). Além disso coloquei o código script dos pop-ups no DashboardEvents.js e tirei o que estava lá porque era de da primeira versão do código, que não estamos utilizando mais.

--- Dashboard/DashboardEvents.js
+++ Dashboard/DashboardEvents.js
@@ -1,8 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const sidebar = document.querySelector('.sidebar');
-
-    menuToggle.addEventListener('click', function() {
-        sidebar.classList.toggle('closed');
+document.querySelectorAll('.MedidasIcone').forEach((icon, index) => {
+    icon.addEventListener('click', () => {
+        const PopupId = ['PopupPotencia', 'PopupCorrente', 'PopupConsumo'][index];
+        showPopup(PopupId);
     });
 });
+
+function showPopup(PopupId) {
+    document.getElementById(PopupId).style.display = 'block';
+}
+
+function hidePopup(PopupId) {
+    document.getElementById(PopupId).style.display = 'none';
+}
