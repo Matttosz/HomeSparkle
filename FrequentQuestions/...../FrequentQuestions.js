ba4c469622065682ca77a7e6127e20fdcba9847a
JVS453 <149633554+JVS453@users.noreply.github.com>
2024-11-27 16:57:35 -0300

    Traduções e conexões com a parte do perfil de usuário.

---
+++ FrequentQuestions/...../FrequentQuestions.js
@@ -0,0 +1,20 @@
+function ShowPopup(PopupId) {
+    // Esconder os pop-ups
+    const Popups = document.querySelectorAll('.Popup');
+    Popups.forEach(Popup => {
+        Popup.classList.remove('show');
+    });
+
+    // Mostrar o pop-up selecionado
+    const Popup = document.getElementById(PopupId);
+    if (Popup) {
+        Popup.classList.add('show');
+    }
+}
+
+function HidePopup(PopupId) {
+    const Popup = document.getElementById(PopupId);
+    if (Popup) {
+        Popup.classList.remove('show'); // Oculta o pop-up
+    }
+}
